dd26ba8a614169816d88e9f2ef97f5417680ef5a
Felipe Fernandes <f.fernandescs@gmail.com>
2022-04-22 23:08:16 -0300

    finalizando responsividade pagina inicial

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,45 +1,67 @@
-const topoPage = document.querySelector('.containerTop'),
-    menuHaburger = document.querySelector('.menuHamburger'),
-    menuNav = document.querySelector('.containerMenu'),
-    btnTop = document.querySelector('.btnTop'),
-    slider = document.querySelectorAll('.carrosel img').length,
-    carroselSlider = document.querySelector('.slider'),
-    carroselImg = document.querySelector('.carrosel'),
-    prevImg = document.querySelector('.prev'),
-    nexstImg = document.querySelector('.next'),
+const btnSliderBack = document.querySelector('.iconBack');
+const btnSliderForward = document.querySelector('.iconForward');
+const imgSlider = document.querySelector('.sliderImg');
+const qtImg = document.querySelectorAll('.sliderImg img');
+const balls = document.querySelector('.balls');
+const btnMenuHamburger = document.querySelector('.menuHamburguer');
 
-    carroselProduct = document.querySelector('.listProduct'),
-    carroselProduct2 = document.querySelector('.listProduct2');
+let indexImgSlider = 0;
 
 
-let indexImg = 0;
-let indexPromo = 0;
-
-let indexLancamentos = 0;
-
-menuHaburger.addEventListener('click', ()=> {   
-    menuNav.classList.toggle('containerMenuActive')
-    if(menuNav.classList.contains('containerMenuActive')) {
-        menuNav.style.visibility = 'visible'
-    } else {
-        menuNav.style.visibility = 'hidden'
-
+window.addEventListener('load', () => {
+    for(let i = 0; i <qtImg.length; i++){
+        const itemBall = document.createElement('span');
+        itemBall.id = i
+        balls.appendChild(itemBall) 
     }
-})
+    const ballImgAtual = document.getElementById('0');
+    ballImgAtual.classList.add('ballsActive')
 
-window.addEventListener('scroll', () =>{
-    if(topoPage.classList.toggle('topPageActive', window.scrollY > 0)){
-        menuHaburger.classList.add('menuHamburgerActive')
-        btnTop.style.visibility = 'visible'
+    const positionBaslls = document.querySelectorAll('.balls span')
+    for(let i = 0; i < positionBaslls.length; i++) {
+        positionBaslls[i].addEventListener('click', () => {
+            indexImgSlider = positionBaslls[i]
+            slider();
+            console.log('clicou')
+        })
+    }
+    
+});
 
+btnSliderBack.addEventListener('click', () => {
+    indexImgSlider--
+    slider();
+});
 
+btnSliderForward.addEventListener('click', () => {
+    indexImgSlider++
+    slider();
+});
+
+function slider() {
+    if(indexImgSlider >= qtImg.length){
+        indexImgSlider = 0
+    } else if(indexImgSlider < 0) {
+        indexImgSlider = qtImg.length -1
+    }
+    imgSlider.style.marginLeft = -1200 * indexImgSlider + 'px'
+    
+
+}
+
+setInterval(() => {
+    indexImgSlider++
+    slider();
+}, 3000)
+
+slider();
+
+btnMenuHamburger.addEventListener('click', () => {
+    const menuResponsivo = document.querySelector('.menu');
+    menuResponsivo.classList.toggle('active')
+    if(menuResponsivo.classList.contains('active')) {
+        btnMenuHamburger.classList.add('active');
     } else {
-        menuHaburger.classList.remove('menuHamburgerActive')
-        btnTop.style.visibility = 'hidden'
-    }    
-})
-
-
-
-
-
+        btnMenuHamburger.classList.remove('active');
+    }
+});
